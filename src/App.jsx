af5b2dafb7b576052b4150f61cef1da6ba762668
Clement Glt <guillemot.clement@protonmail.com>
2024-05-13 21:24:50 +0200

    Refacto avec Reducer()

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,81 @@
-import {useState } from "react"
+import {useReducer} from "react"
 import AddTodo from "./components/AddTodo"
 import TodoList from "./components/TodoList"
 import themeContext from "./context/theme";
+import todoReducer from "./reducers/todoReducer";
 
 
 function App() {
 
-  const [todoList, setTodoList] = useState([]);
+  const [state, dispatch] = useReducer(todoReducer, {
+    theme: 'primary',
+    todoList: []
+  });
 
   function addTodo(content){
-    // on créer la nouvelle todo
-    const todo = {
-      //on jaoute un id pour identifier cette todo
-      //on utilise la méthode pour générer une chaine unique permettant d'identifier la todo
-      id: crypto.randomUUID(),
-      //on ajoute le contenu de la todo
-      content,
-      //par defaut, son etat est pas realiser car on vient se creer une nouvelle todo
-      done: false,
-      //on ajoute un mode edition
-      edit: false,
-      selected: false,
-    }
-    //on vient ajouter la nouvelle todo dans la liste
-    setTodoList([...todoList, todo])
+    dispatch({
+      type: 'ADD_TODO',
+      content
+    });
   }
-
-  // nouvelle fonction qui permet de gérer la suppression
-  //on viendras transmettre la fonction jusqu'a composant enfant TodoItem
+  
   function deleteTodo(id){
-    setTodoList(todoList.filter(todo => todo.id !== id));
+    dispatch({
+      type: 'DELETE_TODO',
+      id
+    });
   }
 
   function toggleTodo(id){
-    setTodoList(todoList.map( todo => todo.id === id ? ({
-      ...todo,
-      done: !todo.done
-    }) : todo));
+    dispatch({
+      type: 'TOGGLE_TODO',
+      id
+    });
   }
 
   function toggleTodoEdit(id){
-    setTodoList(todoList.map( todo => todo.id === id ? ({
-      ...todo,
-      edit: !todo.edit
-    }) : todo));
+    dispatch({
+      type: 'TOGGLE_EDIT_TODO',
+      id
+    });
   }
 
   function editTodo(id, content){
-    setTodoList(todoList.map( todo => todo.id === id ? ({
-      ...todo,
-      edit: false,
+    dispatch({
+      type: 'EDIT_TODO',
+      id,
       content
-    }) : todo ));
+    });
   }
 
   function selectTodo(id){
-    setTodoList(todoList.map( todo => todo.id === id ? ({
-      ...todo,
-      selected: true
-    }) : ({
-      ...todo, 
-      selected: false
-    }) ));
+    dispatch({
+      type: 'SELECT_TODO',
+      id
+    });
   }
 
-  const [theme, setTheme]= useState('primary');
-
   function handleChange(e){
-    setTheme(e.target.value);
+    dispatch({
+      type: 'SET_THEME',
+      theme: e.target.value
+    });
   }
 
   return (
-    <themeContext.Provider value={theme}>
+    <themeContext.Provider value={state.theme}>
     <div className="flex row justify-center align-center p20">
       <div className="card container p20">
         <h1 className="mb20 flex row justify-center align-center">
           <span className="fill">Todo List</span>
-          <select value={theme} onChange={ handleChange}>
+          <select value={state.theme} onChange={ handleChange}>
             <option value="primary">Rouge</option>
             <option value="secondary">Bleu</option>
           </select>
         </h1>
           <AddTodo addTodo={addTodo}/>
           <TodoList 
-            todoList={ todoList } 
+            todoList={ state.todoList } 
             deleteTodo={deleteTodo} 
             toggleTodo={toggleTodo} 
             toggleTodoEdit={toggleTodoEdit}
